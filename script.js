f7f180c5b04f3b4339a69d732407760fdab9847b
maddie <madeleinethornberry@gmail.com>
2022-08-07 20:25:23 -0400

    links and js

---
+++ script.js
@@ -0,0 +1,19 @@
+// date
+var currentDayEl = document.querySelector("#time-display");
+
+dateDisplayEl.textContent = moment();
+
+// event input 
+
+
+
+// save button
+
+
+
+// past and future function
+
+
+
+// 
+
