8a692e34d58a5003c2621454a87ac1ad0a8f104c
maddie <madeleinethornberry@gmail.com>
2022-08-07 21:31:43 -0400

    figuring out how to make the save button work

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 // date
-var currentDayEl = document.querySelector("#time-display");
+var currentDayEl = document.querySelector("#current-day");
 
-dateDisplayEl.textContent = moment();
+currentDayEl = moment();
 
 // event input 
 
 
 
 // save button
+// change later from print - button works so leaving it as is for now
+const saveBtn = document.querySelector(".save-btn")
 
+function saveEvent() {
+    window.print()
+}
 
+saveBtn.addEventListener("click", saveEvent)
+console.log('saved')
 
 // past and future function
 
